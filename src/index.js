903f523fa8dd1f150d83fef8d599f5bb71f30166
Krsmanovic Luka <krsman1996@gmail.com>
2020-04-29 16:48:08 +0200

    Adding redux to project...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
+const store = createStore(reducer);
+
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  passcode: null,
+  lockStatus: "unlocked",
+  statusMessage: "",
+  screenIsOff: false,
+  serialNumber: "4815162342",
+  timerScreenOff: null,
+  timerCheckPasscode: null,
+  timerLocking: null,
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
