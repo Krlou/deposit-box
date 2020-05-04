490db3bbcd235ff8a3485210e695936cf3c9c6bb
Krsmanovic Luka <krsman1996@gmail.com>
2020-05-04 13:39:48 +0200

    Adding persist state to redux

--- src/index.js
+++ src/index.js
@@ -8,10 +8,34 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+};
+
+const persistedState = loadFromLocalStorage();
 const store = createStore(
   reducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 const app = (
   <Provider store={store}>
